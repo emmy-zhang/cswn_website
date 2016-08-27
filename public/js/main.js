a50db093cfe7614b33ad1b5c4962ea07fb10e10d
Emmy <emmy.917.ez@gmail.com>
2016-08-27 17:26:40 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@ $(function () {
   var navHeight = $(".header").outerHeight(true);
   var contentHeight = pageHeight - navHeight;
   $(".content").css("margin-top", navHeight);
-  $(".home-content").css("margin-top", navHeight);
-  $(".contact-content").css("margin-top", navHeight);
+  //$(".home-content").css("margin-top", navHeight);
+  //$(".contact-content").css("margin-top", navHeight);
 })
 
 
