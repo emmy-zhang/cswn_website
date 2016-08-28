4de112236d9d121377fc4f4a921fd5bad73a9049
Emmy <emmy.917.ez@gmail.com>
2016-08-28 13:39:10 -0400

    modal update

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,9 @@ $(function () {
   var navHeight = $(".header").outerHeight(true);
   var contentHeight = pageHeight - navHeight;
   $(".content").css("margin-top", navHeight);
-  //$(".home-content").css("margin-top", navHeight);
-  //$(".contact-content").css("margin-top", navHeight);
-})
 
+
+})
 
 function navDisplay(x) {
   x.classList.toggle("change");
