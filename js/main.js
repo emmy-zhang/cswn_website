5718266595a079ba6418c78c2dcaadf1d18e9186
emmy917917 <emmy.j.zhang@gmail.com>
2016-09-05 18:13:28 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,52 @@
-var modal = document.getElementById('juliannaModal');
-console.log("modal: " + modal);
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+// smooth scroll
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+// change header image
+function changeImage()
+{
+    var img = document.getElementById("home");
+    document.img.style.background="url(img.src)";
+
+    x++;
+
+    if(x >= images.length){
+        x = 0;
+    }
+
+    fadeImg(img, 100, true);
+    setTimeout("changeImage()", 3000);
+}
+
+function fadeImg(el, val, fade){
+    if(fade === true){
+        val--;
+    }else{
+        val ++;
+    }
+
+    if(val > 0 && val < 100){
+        el.style.opacity = val / 100;
+        setTimeout(function(){fadeImg(el, val, fade);}, 10);
     }
 }
+
+var images = [],
+x = 0;
+
+images[0] = "images/officers/group1.jpg";
+images[1] = "images/officers/group2.jpg";
+images[2] = "image3.jpg";
+setTimeout("changeImage()", 3000);
